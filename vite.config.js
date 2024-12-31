6a5d9cd3019a552420e1aa3cd4cfff3ac7c87dcf
Nvnrs <nicolasvenerosy@gmail.com>
2024-12-31 17:25:35 +0400

    correction base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from "@vitejs/plugin-vue";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "/happy_new_year25",
+  base: "/happy-new-year25/",
 });
